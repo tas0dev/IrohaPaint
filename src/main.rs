5ae877e9ef8061ec16a4ed42e3a855c8482e4cb8
tas0dev <156896971+tas0dev@users.noreply.github.com>
2026-07-20 10:58:17 +0900

    ウィンドウを表示できるように

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use viewkit::prelude::*;
+
+struct IrohaPaint;
+
+impl App for IrohaPaint {
+    type Body = Box<dyn View + 'static>;
+
+    fn new() -> Self {
+        Self
+    }
+
+    fn window(&self) -> WindowOptions {
+        WindowOptions::new("IrohaPaint")
+            .size(1280.0, 800.0)
+            .resizable(true)
+    }
+
+    fn body(&self, _context: &ViewContext) -> Box<dyn View + 'static> {
+        Box::new(
+            Background::new()
+                .background(
+                    Rectangle::new()
+                        .color(RectangleColor::Custom(Color::from_rgb_hex(0xf2f2f4))),
+                )
+                .content(
+                    VStack::new()
+                        .alignment(StackAlignment::Center)
+                        .child(
+                            Text::new("IrohaPaint")
+                        ),
+                ),
+        )
+    }
+}
+
+fn main() -> Result<(), ViewKitError> {
+    run::<IrohaPaint>()
 }
